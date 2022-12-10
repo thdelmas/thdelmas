44d81bb796b9fae467d7cb7c23c5177e2b791ac5
Mia <mia@Mia.local>
2022-12-10 16:45:27 +0100

    test

--- public/randomAchievement.php
+++ public/randomAchievement.php
@@ -1,5 +1,5 @@
 <?php
-$string = file_get_contents("./achievements.json");
+$string = file_get_contents("./achievements_2.json");
 $json_a = json_decode($string, true);
 
 $achievement = $json_a[array_rand($json_a)];
