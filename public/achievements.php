d6c87028d944df04607b3edfabdae0bf6a2f9b40
Mia <mia@Mia.local>
2022-12-11 19:28:48 +0100

    add: achievements cards format

--- public/achievements.php
+++ public/achievements.php
@@ -8,16 +8,19 @@ $achievement = json_decode($result, true);
 ?>
 
 <section id="achievements" class="card matrix">
-	<h1>Achievements 42</h1>
+	<h1>Achievements</h1>
 		<article id="achievement">
+		<img src="https://42.fr/wp-content/uploads/2021/05/42-Final-sigle-seul.svg" id="achievement_source_logo" />
+		<div id="achievement_display">
 		<span id="achievement_left">
-			<img src="<?php print "https://api.intra.42.fr" . $achievement["image"]; ?>" />
+			<img src="<?php print "https://api.intra.42.fr" . $achievement["image"]; ?>" id="achievement_logo" />
 		</span>
 		<span id="achievement_right">
 			<h2><?php print $achievement["name"]; ?></h2>
 			<p><?php print $achievement["description"]; ?></p>
 			</article>
 		</span>
+		</div>
 </section>
 
 <script>
@@ -28,7 +31,7 @@ $achievement = json_decode($result, true);
 		xmlHttp.send(null);
 		achievement = JSON.parse(xmlHttp.responseText)
 		document.querySelector("#achievement_right>h2").innerHTML = achievement["name"];
-		document.querySelector("#achievement_left>img").setAttribute("src", "https://api.intra.42.fr" + achievement["image"]);
+		document.querySelector("#achievement_logo").setAttribute("src", "https://api.intra.42.fr" + achievement["image"]);
 		document.querySelector("#achievement_right>p").innerHTML = achievement["description"];
 	}
 	setInterval(changeAchievement, 4242);
