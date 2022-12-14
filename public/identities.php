b9fefbad20d6ef0375e14a194019ecc4e5fbb935
Mia <mia@Mia.local>
2022-12-14 01:00:35 +0100

    rmv: too much shit

--- public/identities.php
+++ public/identities.php
@@ -7,16 +7,5 @@ $result = ob_get_clean();
 $name = $result
 ?>
 <section id="identities" class="card">
-<h1 id="nametitled" class=""><?php echo $name ; ?></h1><h1 class="normal_text">🧑‍💻🌴🚀</h1>
+<h1 id="nametitled" class="">Théophile Delmas</h1><h1 class="normal_text">🧑‍💻🌴🚀</h1>
 </section>
-
-<script>
-	function changeName()
-	{
-		var xmlHttp = new XMLHttpRequest();
-		xmlHttp.open("GET", "/nametitled.php", false);
-		xmlHttp.send(null);
-		document.getElementById("nametitled").innerHTML = xmlHttp.responseText;
-	}
-	setInterval(changeName, 4242);
-</script>
