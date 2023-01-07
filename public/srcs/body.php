989537457a854eb25e9da82d81c70f0b55490bb1
Mia More <mia@ack.ninja>
2023-01-07 11:00:25 +0100

    add: skills

--- public/srcs/body.php
+++ public/srcs/body.php
@@ -1,19 +1,22 @@
 <main>
 <?php 
-	if (preg_match('#/contribute#', $_SERVER['REQUEST_URI'], $matches)) {
-		include "./srcs/contribute.php";
+	if (preg_match('#/$#', $_SERVER['REQUEST_URI'], $matches)) {
+		include "./srcs/home.php";
 	}
-	else if (preg_match('#/achievements#', $_SERVER['REQUEST_URI'], $matches)) {
+	else if (preg_match('#/achievements$#', $_SERVER['REQUEST_URI'], $matches)) {
 		include "./srcs/achievements.php";
 	}
-	else if (preg_match('#/contact#', $_SERVER['REQUEST_URI'], $matches)) {
+	else if (preg_match('#/contact$#', $_SERVER['REQUEST_URI'], $matches)) {
 		include "./srcs/contact.php";
 	}
-	else if (preg_match('#/timeline#', $_SERVER['REQUEST_URI'], $matches)) {
+	else if (preg_match('#/timeline$#', $_SERVER['REQUEST_URI'], $matches)) {
 		include "./srcs/timeline.php";
 	}
-	else if (preg_match('#/$#', $_SERVER['REQUEST_URI'], $matches)) {
-		include "./srcs/home.php";
+	else if (preg_match('#/contribute$#', $_SERVER['REQUEST_URI'], $matches)) {
+		include "./srcs/contribute.php";
+	}
+	else if (preg_match('#/skills$#', $_SERVER['REQUEST_URI'], $matches)) {
+		include "./srcs/skills.php";
 	}
 	else {
 		http_response_code(404);
